6dd85aba9ffdd819c6545f90f3175ea588902f57
Unknown <madjojo70@live.fr>
2018-05-21 00:37:58 +0200

    Add mailbox coordinator

---
+++ Assets/Scripts/Gameplay/MailboxCoordinator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MailboxCoordinator : MonoBehaviour
+{
+    [SerializeField]
+    VacuumMailBox[] m_aVacuumMailboxes;
+
+    TargetFollower m_playerUITarget;
+    GameObject m_player;
+
+    uint m_activeMailbox = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_player = GameObject.FindGameObjectWithTag("Player");
+        m_playerUITarget = m_player.GetComponentInChildren<TargetFollower>();
+        if (m_aVacuumMailboxes.Length != 0)
+            m_playerUITarget.SetTarget(m_aVacuumMailboxes[0].transform);
+        else
+            Debug.LogError("No mailboxes set in Mailbox coordinator");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(m_aVacuumMailboxes[m_activeMailbox].IsDelivered())
+        {
+            if (m_activeMailbox < m_aVacuumMailboxes.Length)
+            {
+
+                m_activeMailbox++;
+                m_aVacuumMailboxes[m_activeMailbox].SetAsCurrentTarget();
+                m_playerUITarget.SetTarget(m_aVacuumMailboxes[m_activeMailbox].transform);
+            }
+            else
+            {
+                // End game
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/UI/TargetFollower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFollower : MonoBehaviour {
+
+    [SerializeField]
+    Transform m_target;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 direction = m_target.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(direction);
+	}
+
+    public void SetTarget(Transform transf)
+    {
+        m_target = transf;
+    }
+}
