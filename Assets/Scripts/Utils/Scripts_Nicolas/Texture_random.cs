d1d3bd6a5a99b6c44198e40b4c59c7d184a0a27c
Nicolas Noël <nico@krismas.net>
2018-05-28 08:44:33 +0200

    MAJ Week-End
    Polish général, préparation de l'application de texture procédurale, réparation partielle des robots.

---
+++ Assets/Scripts/Utils/Scripts_Nicolas/Texture_random.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Texture_random : MonoBehaviour {
+
+	public Material[] Couleurs;
+	public Transform All_buildings;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Color_them_all () {
+		
+		Transform[] Bat_all = All_buildings.GetComponentsInChildren<Transform> ();
+
+		foreach (Transform lebat in Bat_all) {
+
+			if (lebat.name == "Batiment") {
+
+				lebat.GetComponent<MeshRenderer> ().material = lebat.GetComponent<Texture_random>().Couleurs[Random.Range (0, Couleurs.Length)];
+
+			}		
+		}
+	}
+}
