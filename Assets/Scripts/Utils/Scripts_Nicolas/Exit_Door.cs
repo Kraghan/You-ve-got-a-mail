7b1d4360ec7b7050eaad47d94eee024ce32dfd10
Nicolas Noël <nico@krismas.net>
2018-06-04 09:07:39 +0200

    MAJ Lundi matin 1
    Mise à jour tuto pour l'orientation et la sortie fonctionnelle en théorie.

---
+++ Assets/Scripts/Utils/Scripts_Nicolas/Exit_Door.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit_Door : MonoBehaviour {
+
+	public Collider Opener;
+	public Transform thedoor;
+	public float height;
+	public float speed;
+
+	private float originalheight;
+	private bool opened;
+
+	void Start () {
+
+		originalheight = thedoor.transform.position.y;
+
+	}
+
+	void Openthedoor () {
+
+		if (thedoor.transform.position.y <= originalheight + height) {
+
+			thedoor.transform.position += Vector3.up * speed * Time.deltaTime;
+
+		}
+	}
+
+	void Closethedoor () {
+
+		if (thedoor.transform.position.y >= originalheight) {
+
+			thedoor.transform.position -= Vector3.up * speed * Time.deltaTime;
+
+		}
+	}
+
+	void OnTriggerEnter (Collider player) {
+
+		if (player.gameObject.tag == "Player") {
+			
+			opened = true;
+
+		}
+
+	}
+
+	void OnTriggerExit (Collider playertoo) {
+
+		if (playertoo.gameObject.tag == "Player") {
+			
+			opened = false;
+
+		}
+
+	}
+
+	void Update () {
+
+		if (opened)
+			Openthedoor ();
+		else
+			Closethedoor ();
+
+	}
+}
